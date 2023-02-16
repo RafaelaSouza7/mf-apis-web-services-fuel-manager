aeaf4599672261aa97460830adb422aa93d4aa4e
Rafaela <rafaelasouza2207@hotmail.com>
2023-02-15 22:40:54 -0300

    Configuração do Entity Framework

---
+++ mf-apis-web-services-fuel-manager/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo> Consumo { get; set; }
+    }
+}
